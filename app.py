87323f6efd05be04ac5e31d8fefda7a6dd43de55
adnbal <99461328+adnbal@users.noreply.github.com>
2025-07-13 11:00:58 +1200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
 
+# train_model.py
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import joblib
+
+df = pd.read_csv("trademe_data.csv")
+X = df[["bedrooms", "bathrooms", "floor_area", "land_area"]]
+y = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = RandomForestRegressor()
+model.fit(X_train, y_train)
+
+joblib.dump(model, "model.pkl")
